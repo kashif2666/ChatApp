d89850d4b0cea20c3a26ab795ce9d9d77fb54f02
kashif2666 <kashifjsr7@gmail.com>
2024-07-04 00:43:01 +0530

    Part:3 Completed , Connected Backend from frontend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TbMessages } from "react-icons/tb";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // cleanup function
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* Header  */}
           <div className="bg-slate-500 px-4 py-2 mb-2 flex float-start  ">
             <span className="label-text text-white">To : &nbsp; </span>{" "}
-            <span className="text-gray-900 font-bold "> John Doe</span>
+            <span className="text-gray-900 font-bold ">
+              {selectedConversation.fullName}
+            </span>
           </div>
 
           <Messages />
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,16 +1,34 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectGender }) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className={`label gap-2 cursor-pointer`}>
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text text-white">Male</span>
-          <input type="checkbox" className="checkbox bg-white" />
+          <input
+            type="checkbox"
+            className="checkbox bg-white"
+            checked={selectGender == "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className={`label gap-2 cursor-pointer`}>
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text text-white">Female</span>
-          <input type="checkbox" className="checkbox bg-white" />
+          <input
+            type="checkbox"
+            className="checkbox bg-white"
+            checked={selectGender == "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
